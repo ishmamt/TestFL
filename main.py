2ee46b3e69f43e201515703cf0439c3ef3fe21cd
Ishmam Tashdeed <ishmam.tashdeed13@gmail.com>
2024-05-08 23:12:59 +0600

    Started adding strategy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import hydra
 from omegaconf import OmegaConf, DictConfig
+import flwr as fl
 
 from dataset import load_dataset
 from client import generate_client_function
@@ -14,7 +15,10 @@ def main(cfg: DictConfig):
     
     # Define Clients
     client_function = generate_client_function(train_loaders, val_loaders, cfg.num_classes)
-
-
+    
+    # Define Strategy
+    strategy = fl.server.strategy.FedAVG(fraction_fit=)
+    
+    
 if __name__ == "__main__":
     main()
